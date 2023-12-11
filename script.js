1f9b3686f86e646ab586e74051e05e84ad97c48f
Daniel Crespo <dani.crespo.rodriguez@gmail.com>
2023-12-11 21:05:14 +0100

    Cabecera meta nueva

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.querySelector("div").textContent=window.navigator.userAgentData.platform;
 document.writeln("navigator.userAgent: "+window.navigator.userAgent);
-        document.writeln("navigator.userAgentData.platform:" +window.navigator.userAgentData.platform);
-        document.writeln("navigator.platform: "+window.navigator.platform);
-        document.writeln("touch: "+navigator.maxTouchPoints);
+document.writeln("navigator.userAgentData.platform:" +window.navigator.userAgentData.platform);
+document.writeln("navigator.platform: "+window.navigator.platform);
+document.writeln("touch: "+navigator.maxTouchPoints);
