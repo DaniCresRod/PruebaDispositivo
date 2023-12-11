42bd18bc3c70643951cd001ecc3175c2d6445ab8
Daniel Crespo <dani.crespo.rodriguez@gmail.com>
2023-12-11 21:15:29 +0100

    sin "window"

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ document.writeln("navigator.userAgent: "+window.navigator.userAgent);
 document.writeln("navigator.userAgentData.platform:" +window.navigator.userAgentData.platform);
 document.writeln("navigator.platform: "+window.navigator.platform);
 document.writeln("touch: "+navigator.maxTouchPoints);
+document.querySelector("div").textContent=navigator.maxTouchPoints;
